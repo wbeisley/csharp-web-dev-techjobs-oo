7fda6ddd40cb035c0702249b324c0e22f99eed5f
wbeisley <wbeisley@gmail.com>
2021-10-20 20:04:17 -0400

    Finished up to Job Class

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -13,7 +13,31 @@ namespace TechJobsOO
         public CoreCompetency JobCoreCompetency { get; set; }
 
         // TODO: Add the two necessary constructors.
+        public Job()
+        {
+            Id = nextId;
+            nextId++;
+        }
 
+        public Job(string name, object employerName, object employerLocation, object jobType, object jobCoreCompetency)
+        {
+            Name = name;
+            EmployerName = employerName;
+            EmployerLocation = employerLocation;
+            PositionType = jobType;
+            CoreCompetency = jobCoreCompetency;
+        }
         // TODO: Generate Equals() and GetHashCode() methods.
+        
+        public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
     }
 }
